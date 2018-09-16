69e552917f730fa282f561077198f0619753f23b
Harrison Turley <harrison.turley.1998@gmail.com>
2018-09-15 22:43:13 -0700

    Updated such that react components appear

--- Website/react-client/src/Components/HomePage.jsx
+++ Website/react-client/src/Components/HomePage.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 class HomePage extends Component {
   render() {
     return (
       <div>
-        Home page
+        Test
       </div>
     )
   }
 }
+
+/*function tick() {
+  const element = (
+    <div> 
+      <h2>Hi!</h2>
+      <h2>The time is: {new Date().toUTCString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(element, document.getElementById('time'));
+}*/
+
+//setInterval(HomePage.render(), 1000);
+
 export default HomePage;
